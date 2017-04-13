662208d18c09443aad6e9476f791bd25a477f6e7
waschbar01 <aquan@macalester.edu>
2017-04-12 19:16:10 -0500

    node.js server file import

---
+++ routes/recipes.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Tianyou on 2017/3/30.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('viciousdelicious:cake50@ds155080.mlab.com:55080/heroku_xdjx3gtb');
+
+router.get('/', function(req, res) {
+    var collection = db.get('recipes');
+    collection.find({}, function(err, recipes){
+        if (err) throw err;
+        res.json(recipes);
+    });
+});
+
+module.exports = router;
